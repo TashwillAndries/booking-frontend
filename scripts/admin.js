94cae43110c8d5e4714a10e473ed239897385361
TashwillAndries <tashwilla27@gmail.com>
2021-09-07 14:01:05 +0200

    new updates

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,18 +1,22 @@
-fetch("https://serene-basin-92650.herokuapp.com/show-appointments/")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
+mystorage = window.localStorage;
 
-    let tableBody = document.getElementById("tableData");
-    let dataBody = "";
-    data["data"].forEach((person) => {
-      dataBody += `<tr><td>${person[0]}</td><td>${person[5]}</td><td>${person[4]}</td><td>${person[3]}</td>
+function renderData(records) {
+  fetch("https://serene-basin-92650.herokuapp.com/show-appointments/")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      let tableBody = document.getElementById("tableData");
+      let dataBody = "";
+      data["data"].forEach((person) => {
+        dataBody += `<tr><td>${person[0]}</td><td>${person[5]}</td><td>${person[4]}</td><td>${person[3]}</td>
                   <td>${person[2]}</td><td>${person[6]}</td><td>${person[7]}</td></tr>
                   `;
-    });
+      });
 
-    tableBody.innerHTML += dataBody;
-  });
+      tableBody.innerHTML += dataBody;
+    });
+}
 
 let modalBtn = document.querySelector(".btn-danger");
 let modalBg = document.querySelector(".modal-bg");
@@ -60,6 +64,11 @@ updateModalClose.addEventListener("click", function () {
 function updateAppoint() {
   let id = document.getElementById("appointment_id").value;
   console.log(id);
+  let dateOne = new Date(document.querySelector("#check_in_date").value);
+  let dateTwo = new Date(document.querySelector("#check_out_date").value);
+  let difference = dateTwo.getTime() - dateOne.getTime();
+  let days = 1000 * 3600 * 24;
+  let results = difference / days;
   fetch(`https://serene-basin-92650.herokuapp.com/update-appointment/${id}`, {
     method: "PUT",
     body: JSON.stringify({
@@ -67,9 +76,19 @@ function updateAppoint() {
       check_out_date: document.getElementById("check_out_date").value,
       appointment_user: document.getElementById("username").value,
       hotel_name: document.getElementById("hotel_name").value,
+      total: parseInt(mystorage.getItem("price")) * results,
     }),
     headers: {
       "Content-type": "application/json",
     },
   });
 }
+
+const url = "https://serene-basin-92650.herokuapp.com/show-appointments/";
+let records = [];
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => {
+    records = data.data;
+    renderData(records);
+  });
